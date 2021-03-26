02e4488b7a9374c3715901c177a3cc68b078984d
Siehan <memain.ye.sylvie@gmail.com>
2021-03-26 19:36:02 +0100

    done

---
+++ isEven2.js
@@ -0,0 +1,20 @@
+/*
+isEven.js
+
+Ecrire une fonction isEven qui prend un nombre en paramètre et qui devra retourner true
+si le nombre passé en paramètre est pair, sinon la fonction retournera false.
+*/
+
+const isEven = (nb) => {
+  if (nb % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(isEven(7)); // OUTPUT : false
+//console.log(isEven(3)); // OUTPUT : false
+//console.log(isEven(6)); // OUTPUT : true
+
+//if (nb % 2 !== 0)

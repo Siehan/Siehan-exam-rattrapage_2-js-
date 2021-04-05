7fbc6ae100d58d0bc5f091fb2f374510dfcefbc4
Siehan <memain.ye.sylvie@gmail.com>
2021-04-06 00:58:27 +0200

    done & update

--- threeWayLoop2.js
+++ threeWayLoop2.js
@@ -18,20 +18,21 @@ Alice, length: 5, index: 0
 
 const chalk = require("chalk");
 
-// Pas réussi à afficher les index des 3 boucles dans l'odre inversé
-
-let tab = ["Alice", "Bob", "Charlie", "Dan", "Eve"].reverse();
+let tab = ["Alice", "Bob", "Charlie", "Dan", "Eve"];
 
 console.log(chalk.red("---------- FOR LOOP ----------"));
 
-for (let i = 0; i < tab.length; ++i) {
+for (let i = tab.length - 1; i >= 0; i--) {
   console.log(chalk.yellow(`${tab[i]}, length: ${tab[i].length}, index: ${i}`));
 }
 
+// Pas réussi à afficher les index des 2 boucles dans l'odre inversé
+
+let tab2 = ["Alice", "Bob", "Charlie", "Dan", "Eve"].reverse();
 console.log(chalk.red("---------- FOR OF LOOP ----------"));
 
 let i = 0;
-for (let element of tab) {
+for (let element of tab2) {
   console.log(chalk.green(`${element}, length: ${element.length}, index: ${i}`));
   ++i;
 }
@@ -46,11 +47,11 @@ tab.forEach((element, index) => {
 OUTPUT :
 
 ---------- FOR LOOP ----------
-Eve, length: 3, index: 0
-Dan, length: 3, index: 1
+Eve, length: 3, index: 4
+Dan, length: 3, index: 3
 Charlie, length: 7, index: 2
-Bob, length: 3, index: 3
-Alice, length: 5, index: 4
+Bob, length: 3, index: 1
+Alice, length: 5, index: 0
 ---------- FOR OF LOOP ----------
 Eve, length: 3, index: 0
 Dan, length: 3, index: 1
@@ -58,10 +59,10 @@ Charlie, length: 7, index: 2
 Bob, length: 3, index: 3
 Alice, length: 5, index: 4
 ---------- FOR EACH LOOP ----------
-Eve, length: 3, index: 0
-Dan, length: 3, index: 1
+Alice, length: 5, index: 0
+Bob, length: 3, index: 1
 Charlie, length: 7, index: 2
-Bob, length: 3, index: 3
-Alice, length: 5, index: 4
+Dan, length: 3, index: 3
+Eve, length: 3, index: 4
 
 */
